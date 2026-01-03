cc9cc9cc73d11cbcc2fc7301b3b48b69f5b2349e
Facundo Castro <facundo.julian.castro@gmail.com>
2026-01-02 21:16:03 -0300

    Cambios en los prints para gacerlo más amigable con el usuario, cambio en el descuento de vidas

--- juego-adivinanza.py
+++ juego-adivinanza.py
@@ -2,25 +2,24 @@ import random
 
 numeroSecreto = random.randint(1, 101)
 adivinado = False
-vidas = 5 #me pareció más simple con un sistema de vidas y que reduzca en 1 por cada error
+vidas = 3 # me pareció más simple con un sistema de vidas y que reduzca en 1 por cada error
 
-print("Bienvenido al juego")
+# print(numeroSecreto) -> Esto fue solo para adivinar y probar todo
+print("¡Bienvenido al juego de adivinanzas!")
 
 while not adivinado:
     if vidas == 0: 
-        print(f"Game over, el numero era {numeroSecreto}") #al perder, me pareció bueno que muestre el número a adivinar
+        print(f"¡Game over! El numero era {numeroSecreto}") # al perder, me pareció bueno que muestre el número a adivinar
         break
+    numero = int(input("Ingresá un numero del 1 al 99: "))
+    if numero == numeroSecreto:
+        print("¡Adivinaste!")
+        adivinado = True
+    elif numero < numeroSecreto:
+        print("Tu número es mas bajo")
+    elif numero > numeroSecreto:
+        print("Tu número es mas alto")
     else:
-        numero = int(input("introduzca un numero del 1 al 99: "))
-        if numero == numeroSecreto:
-            print("Adivinaste")
-            adivinado = True
-        elif numero < numeroSecreto:
-            print("Tu número es mas bajo, volve a intentarlo")
-            vidas -=1
-        elif numero > numeroSecreto:
-            print("Tu número es mas alto, volve a intentarlo")
-            vidas -=1
-        else:
-            print("hubo un error") #TODO manejo de errores, ejemplo: se ingresa texto
-    
+        print("Hubo un error") #TODO manejo de errores, ejemplo: se ingresa texto
+    vidas -=1
+    print(f"Te quedan {vidas} vidas")
